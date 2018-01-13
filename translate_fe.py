8a4b4eeac5ebb451c0d3ea37b0b893308b802223
oriany <akhilbirlangi@gmail.com>
2018-01-13 23:19:02 +0000

    Fix bugs, add multiple scripts

--- translate_fe.py
+++ translate_fe.py
@@ -1,19 +1,30 @@
 from gtts import gTTS
 import os
+import time
 from googletrans import Translator
 
 file_object=open("a.txt","r+")
 
 translator=Translator()
 
-translations=translator.translate(file_object.read(), dest='en')
+textVal = file_object.read()
+
+langs=translator.detect(textVal)
+
+translations=translator.translate(textVal, dest='en')
 
 print ("***Translate File: ")
 print (translations.text)
 
-tts_2 = gTTS(text = file_object.read(), lang = 'en')
+tts_2 = gTTS(text = textVal, lang =langs.lang)
 tts_2.save("aud2.mp3")
-os.system("mpg321 aud.mp3")
+
 tts = gTTS(text = translations.text, lang = 'en')
 tts.save("aud.mp3")
+
+#Original foreign language speech
+os.system("mpg321 aud2.mp3")
+time.sleep(1)
+
+#Translated English speech
 os.system("mpg321 aud.mp3")
